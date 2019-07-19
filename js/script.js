85d082c7935e84a382d6764a1c9d14f9be303023
daisyarusey <daisyarusey.dc@gmail.com>
2019-07-20 02:20:00 +0300

    Add jQuery function to alert user after submitting the form

--- js/script.js
+++ js/script.js
@@ -11,12 +11,16 @@ $("#dev,#dev-hide").click(function(){
     $("#prod, #prod-hide").toggle();
     prod
 });
-});
 
-$(document).ready(function() {
-  $('.image-frame').hover(function(){
+$('.image-frame').hover(function(){
   $('.image-caption',this).slideToggle('slow');
 }, function(){
   $('.image-caption',this).slideToggle('slow');
   });
+
+  $("form").submit(function(){
+    event.preventDefault();
+    alert("Thank you" + " " + document.getElementById('name').value);
+  });
 });
+
