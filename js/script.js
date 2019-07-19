1106d8d99664e2eb152cb977867e406af8348ce6
daisyarusey <daisyarusey.dc@gmail.com>
2019-07-19 17:46:59 +0300

    Add jquery function to toggle design,dev and prod

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
+$("#design,#design-hide").click(function(){
+$("#design, #design-hide").toggle();
+});
+
 $("#dev,#dev-hide").click(function(){
-$("#dev, #dev-hide").toggle();
+  $("#dev, #dev-hide").toggle();
+  });
+
+  $("#prod,#prod-hide").click(function(){
+    $("#prod, #prod-hide").toggle();
+    prod
 });
 });
