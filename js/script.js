7ad22e9ba3248cf56384ff7c3d8c7449a4c664b0
daisyarusey <daisyarusey.dc@gmail.com>
2019-07-20 00:06:26 +0300

    Add function jquery for hoover function

--- js/script.js
+++ js/script.js
@@ -13,3 +13,10 @@ $("#dev,#dev-hide").click(function(){
 });
 });
 
+$(document).ready(function() {
+  $('.image-frame').hover(function(){
+  $('.image-caption',this).slideToggle('slow');
+}, function(){
+  $('.image-caption',this).slideToggle('slow');
+  });
+});
